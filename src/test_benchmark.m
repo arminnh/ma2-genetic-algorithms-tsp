543276c4728950f869403519d19ad4486b256c69
Armin <halilovic.armin@hotmail.com>
2018-01-08 00:24:15 +0100

    Added better performing benchmark parameters

--- src/test_benchmark.m
+++ src/test_benchmark.m
@@ -1,9 +1,9 @@
 NIND=1000;           % Number of individuals
 MAXGEN=1000;         % Maximum no. of generations
-ELITIST=0.05;        % percentage of the elite population
+ELITIST=0.10;        % percentage of the elite population
 STOP_PERCENTAGE=.95; % percentage of equal fitness individuals for stopping
-PR_CROSS=.50;        % probability of crossover
-PR_MUT=.30;          % probability of mutation
+PR_CROSS=.35;        % probability of crossover
+PR_MUT=.50;          % probability of mutation
 LOCALLOOP=1;         % local loop removal
 CROSSOVER = 'cross_order'; % crossover operators
 MUTATION = 'mut_inversion'; % mutation operators
@@ -11,7 +11,7 @@ SELECTION = 'sel_tournament'; % parent selection algorithm
 SUBPOP = 1;          % Amount of subpopulations
 CUSTOMSTOP = 0;      % Custom stopping criterion on/off
 CUSTOMSS = 1;        % Custom survivor selection on/off
-RUNS = 5;            % Number of ga runs in tests
+RUNS = 2;            % Number of ga runs in tests
 
 datasetslist = dir('tsp-benchmark-problems/');
 Ndatasets = size(datasetslist, 1) - 2;
